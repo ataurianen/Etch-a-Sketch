72a1b7718f69d2e6fe472db8e3ae0cc5f7250d38
Andrew Taurianen <andrew.taurianen@gmail.com>
2023-01-27 15:33:38 -0500

    Added button to clear the grid and change the size

--- README.md
+++ README.md
@@ -3,8 +3,7 @@
 TOP Etch-A-Sketch.
 
 Makes a 16x16 grid and when you mouse over each square, it will color it black.
+Added button to clear the grid and ask what size for a new grid.
 
 #TODO
--Variable size grids
--Resetting the grid with a button
 -Make it so it only inceases the darkness by 10% each time the mouse is dragged over it
--- javascript.js
+++ javascript.js
@@ -1,19 +1,46 @@
-const container = document.getElementById("container");
-var gridSize = 16;
-
 //Need to create a 16x16 squard of divs
+const container = document.getElementById("container");
 
-for (let rows = 0; rows < gridSize; rows++){
-    for (let columns = 0; columns < gridSize; columns++){
-        let div = document.createElement('div');
-        div.classList.add('grid');
-        container.append(div);
+function createGrid(gridSize) {
+    for (let rows = 0; rows < gridSize; rows++){
+        for (let columns = 0; columns < gridSize; columns++){
+            let div = document.createElement('div');
+            div.setAttribute('style', 'width:' + 960/gridSize + 'px; height:' + 960/gridSize + 'px');
+            div.classList.add('grid');
+            container.append(div);
+        }
     }
-    document.getElementsByClassName('grid')
+    addEventListenerToGrid();   
 }
 
-const grids = document.querySelectorAll('.grid');
-grids.forEach(grid => grid.addEventListener('mouseover', function(){
+function makeNewGrid () {
+    let newGridSize = prompt("How many boxes per row? (no more than 100)");
+    while (newGridSize > 100) {
+        newGridSize = prompt("How many boxes per row? (no more than 100)");
+    }
+    clearGrid();
+    createGrid(newGridSize);
+}
+
+function clearGrid () {
+    const grid = container.getElementsByClassName('grid');
+    while (grid.length > 0) {
+        grid[0].parentNode.removeChild(grid[0]);
+    }
+}
+
+function addEventListenerToGrid() {
+    //adds event listener
+    const grids = document.querySelectorAll('.grid');
+
+    grids.forEach(grid => grid.addEventListener('mouseover', function(){
     grid.classList.add('hovered');
 }));
+}
 
+//Makes the initial 16x16 grid when page loads
+window.onload = createGrid(16);
+
+document.getElementById('new-grid-btn').addEventListener('click', function() {
+    makeNewGrid();
+});
